688ed9c8b210459dbfde9274ad2e1ca39f235c2e
shivam.red <shivamdwivedi618@gmail.com>
2023-12-06 20:09:49 +0530

    new chnages

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 // server.js
 const express = require('express');
+const cors = require('cors') 
 const app = express();
 const routes = require('./routes');
 const { connectToDatabase } = require('./db'); // Import connectToDatabase function from db.js
 
 app.use(express.json()); // Middleware to parse JSON request bodies
+app.use(cors())
 
 // Use the routes defined in routes.js
 app.use('/api', routes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4200;
 
 connectToDatabase() // Initialize database connection when server starts
   .then(() => {
